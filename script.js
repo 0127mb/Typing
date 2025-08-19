057f88d13f43de0c07e81c516f5722f3e6426eb1
0127mb <muhammedjamolidinov@gmail.com>
2025-08-19 13:57:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let h1  = document.querySelector('h1')
+let h2  = document.querySelector('h2')
+let typed  = new Typed(h1,{
+    strings:["Text Gradients"],
+    typedSpeed:50,
+    backSpeed:25,
+    loop:true
+})
+let typed1  = new Typed(h2,{
+    strings:["Make your headlines stand out"],
+    typedSpeed:50,
+    backSpeed:25,
+    loop:true
+})
